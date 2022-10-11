6e4e1cc0f1584f44466f6e1f4d4709463e8ef46c
Ackim Brian <kimbrian2010@gmail.com>
2022-10-12 00:54:11 +0200

    added index and base template

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def blog_list(request):
+    return render(request, 'blog_app/blog_list.html', context={})
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/',include('blog_login.urls')),
+    path('blog/',include('blog_app.urls')),
+    path('', views.index, name='index')
 ]
