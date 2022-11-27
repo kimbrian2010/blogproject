ea54f518634639f699e282740cebd2d6d149e3f9
Ackim Brian <kimbrian2010@gmail.com>
2022-11-27 21:13:22 +0200

    Full features

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('', views.BlogList.as_view(), name='blog-list' ),
     path('write/', views.CreateBlog.as_view(), name='create-blog' ), #Use 'write/' as path
     path('details/<int:pk>', views.blog_details, name='blog-details' ),
+    path('liked/<pk>', views.liked, name='liked-post' ),
+    path('unliked/<pk>/', views.unliked, name='unliked-post' ),
+    path('my-blog/', views.MyBlog.as_view(), name='my-blog' ),
+    path('edit-blog/<pk>', views.UpdateBlog.as_view(), name='edit-blog' ),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -14,6 +14,21 @@ from .forms import CommentForm
 #def blog_list(request):
     #return render(request, 'blog_app/blog_list.html', context={})
 
+    
+
+class MyBlog(LoginRequiredMixin,TemplateView):
+    template_name = 'blog_app/my_blog.html'
+    
+    
+class UpdateBlog(LoginRequiredMixin, UpdateView):
+    model = Blog
+    fields = ('blog_title','blog_content', 'blog_image')
+    template_name = 'blog_app/blog_edit.html'
+    
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('blog_app:blog-details', kwargs={'pk':self.object.pk})
+    
+
 
 class BlogList(ListView):
     context_object_name = 'blogs' #passing context in class based view
@@ -40,6 +55,12 @@ def blog_details(request, pk):
     blog = Blog.objects.get(pk=pk)
     
     comment_form = CommentForm()
+    already_like = Likes.objects.filter(blog=blog, user=request.user)
+    if already_like:
+        liked = True
+    else:
+        liked = False
+    
     if request.method == 'POST':
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
@@ -47,7 +68,25 @@ def blog_details(request, pk):
             comment.user = request.user
             comment.blog = blog # blog is a ForeignKey in Comment Comment Model connecting to Blog model
             comment.save()
-            return HttpResponseRedirect(reverse('blog_app:blog-details', kwargs={'pk':pk})) #Return to details page with correct pk(id)
-            
-        
-    return render(request, 'blog_app/blog_details.html', context={'blog':blog, 'comment_form':comment_form}) #pass Blog and CommentForm context here
+            return HttpResponseRedirect(reverse('blog_app:blog-details', kwargs={'pk':pk})) #Return to details page with correct pk(id)      
+    return render(request, 'blog_app/blog_details.html', context={'blog':blog, 'comment_form':comment_form, 'liked':liked}) #pass Blog and CommentForm context here
+
+
+@login_required
+def liked(request, pk):
+    blog = Blog.objects.get(pk=pk)
+    user = request.user
+    already_liked = Likes.objects.filter(blog=blog, user=user)
+    if not already_liked:
+        liked_post = Likes(blog=blog, user=user)
+        liked_post.save()
+    return HttpResponseRedirect(reverse('blog_app:blog-details', kwargs={'pk':blog.pk}))
+
+
+@login_required
+def unliked(request,pk):
+    blog = Blog.objects.get(pk=pk)
+    user = request.user
+    already_liked = Likes.objects.filter(blog=blog, user=user)
+    already_liked.delete()
+    return HttpResponseRedirect(reverse('blog_app:blog-details', kwargs={'pk':blog.pk}))
